39cfde875517cc42f9f7b21b0ebdd905c66f3b05
dipanja <worldofbappa@gmail.com>
2024-10-21 02:32:46 +0530

    nvim: keymap for session init in obsession

--- nvim/lua/bappa/plugins/obsession.lua
+++ nvim/lua/bappa/plugins/obsession.lua
@@ -1,6 +1,7 @@
 return {
 	"tpope/vim-obsession",
 	config = function()
-		vim.keymap.set("n", "<leader>ss", ":source Session.vim <cr>", { desc = "load Session" })
+		vim.keymap.set("n", "<leader>ss", ":source Session.vim<cr>", { desc = "load Session" })
+		vim.keymap.set("n", "<leader>ms", ":Obsession<cr>", { desc = "Session init" })
 	end,
 } -- session management
