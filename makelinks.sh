895e7b4e076af8e7c7fe79187090631ffa8afd60
dipanja <worldofbappa@gmail.com>
2024-10-31 03:53:13 +0530

    raname make.sh to makelinks.sh

--- makelinks.sh
+++ makelinks.sh
@@ -11,6 +11,10 @@ echo "Symlink created for kitty folder."
 ln -sfn "$SOURCE_DIR/nvim" "$DEST_DIR/.config/nvim"
 echo "Symlink created for nvim folder."
 
+# Create symlink for ranger
+ln -sfn "$SOURCE_DIR/ranger" "$DEST_DIR/.config/ranger"
+echo "Symlink created for ranger."
+
 # Create symlink for .zshrc file
 ln -sfn "$SOURCE_DIR/zsh/.zshrc" "$DEST_DIR/.zshrc"
 echo "Symlink created for .zshrc file."
@@ -18,5 +22,9 @@ echo "Symlink created for .zshrc file."
 # Create symlink for .tmux.conf file
 ln -sfn "$SOURCE_DIR/tmux/.tmux.conf" "$DEST_DIR/.tmux.conf"
 echo "Symlink created for .tmux.conf file."
+
+# Create symlink for .fzf.zsh file
+ln -sfn "$SOURCE_DIR/fzf/fzf.zsh" "$DEST_DIR/.fzf.zsh"
+echo "Symlink created for .fzf.zsh file."
 
 echo "All symlinks have been created successfully."
