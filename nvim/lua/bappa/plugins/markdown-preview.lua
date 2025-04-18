924131b239bc0885d16982272338109ceb5f8d64
dipanja <worldofbappa@gmail.com>
2025-04-18 19:20:51 +0530

    nvim markdowm preview cleanup

--- nvim/lua/bappa/plugins/markdown-preview.lua
+++ nvim/lua/bappa/plugins/markdown-preview.lua
@@ -2,9 +2,6 @@ return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
-	-- build = function()
-	-- 	vim.fn["mkdp#util#install"]()
-	-- end,
 	build = "cd app && npm install",
 	config = function()
 		vim.keymap.set(
